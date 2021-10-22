4bd0d888c298bce3f29da38f7f0d2ca020691a45
kevin3033 <andreykevinofc@gmail.com>
2021-10-22 00:30:25 -0300

    login - parte esquerda

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
+import './_app'
+import {  } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import style from '../styles/Home.module.css'
+import logo from '../assets/logo.svg'
 const App = () => {
     return (
-        <div>
-            <h1>Olá mundo</h1>
-        </div>
+        <>
+            <div className={style.left}>
+                <div className={style.cardleft}>
+                    <img className={style.logo} src={logo.src} alt='Facebook'/>
+                    <h2 className={style.textl}>Connect with friends and the world around you on Facebook.</h2>
+                </div>
+            </div>
+            <div className={style.right}>
+                <div className={style.cardright}>
+
+                </div>
+            </div>
+        </>
     )
 }
 
